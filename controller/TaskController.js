e76cf62e97be6e0113c972f3a9cad60ca8f14b6e
KelvinGuimaraes <kelvingui18@gmail.com>
2024-03-30 13:50:44 -0300

    Config: GetById, Update and Delete

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -3,7 +3,7 @@ const Task = require("../models/Task");
 const getAllTasks = async (req, res) => {
   try {
     const tasksList = await Task.find();
-    return res.render("index", {tasksList});
+    return res.render("index", { tasksList, task: null, taskDelete: null });
   } catch (err) {
     res.status(500).send({ error: err.message });
   }
@@ -24,7 +24,49 @@ const createTask = async (req, res) => {
   }
 };
 
+const getById = async (req, res) => {
+  try {
+    const tasksList = await Task.find();
+    const taskId = req.params.id;
+
+    if (req.url.includes("update")) {
+      const task = await Task.findOne({ _id: taskId });
+      res.render("index", { task, taskDelete: null, tasksList });
+    } else if (req.url.includes("delete")) {
+      const taskDelete = await Task.findOne({ _id: taskId });
+      res.render("index", { task: null, taskDelete, tasksList });
+    } else {
+      // Handle other cases or return an error
+      res.status(404).send("Route not found");
+    }
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
+
+const updateOneTask = async (req, res) => {
+  try {
+    const task = req.body;
+    await Task.updateOne({ _id: req.params.id }, task);
+    res.redirect("/");
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
+
+const deleteOneTask = async (req, res) => {
+  try {
+    await Task.deleteOne({ _id: req.params.id });
+    res.redirect("/");
+  } catch {
+    res.status(500).send({ error: err.message });
+  }
+};
+
 module.exports = {
   getAllTasks,
   createTask,
+  getById,
+  updateOneTask,
+  deleteOneTask,
 };
